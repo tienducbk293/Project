4d2ffd98fb376a97d17c762ed274008966ba56a9
tienducbk293 <tienducbk293@gmail.com>
2018-05-04 09:25:58 +0700

    Edit color 2

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,21 +1,21 @@
 <!-- Start Header Area -->
 <header class="default-header">
-	<nav class="navbar navbar-expand-lg navbar-light">
-		<div class="container">
-	        <a class="navbar-brand" href="{{route('homepage')}}">
-		        <img style = "height: 40px; width: 90px" src="source/assets/img/environment_logo.jpg" alt="">
-	        </a>
-	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			    <span class="navbar-toggler-icon"></span>
-	        </button>
+	<nav class="navbar navbar-expand-lg navbar-light nav-pills">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="{{route('homepage')}}">
+		        	<img style = "height: 40px; width: 90px" src="source/assets/img/environment_logo.jpg" alt="">
+	        	</a>
+	        	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    	<span class="navbar-toggler-icon"></span>
+	        	</button>	
+			</div>
 
-	        <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
-			    <ul class="navbar-nav">
-	        <li><a href="{{route('homepage')}}#home">Home</a></li>
-	        <li><a href="{{route('homepage')}}#chart">Chart</a></li>
-	        <li><a href="{{route('homepage')}}#maps">Map</a></li>
-	        <li><a href="{{route('homepage')}}#team">Contact</a></li>
-	        </div>						
+			<ul class="nav navbar-nav" style="color: white">
+	       		<li><a href="{{route('homepage')}}">Home</a></li>
+	        	<li><a href="{{route('homepage')}}#cat">Infomation</a></li>
+	        	<li><a href="{{route('homepage')}}#team">Contact</a></li>
+	        </ul>					
 		</div>
 	</nav>
 </header>
